e8c9454ba79b5a54c857c181d3363a8641984aeb
abidencube14-art <abidencube14@gmail.com>
2026-07-22 16:23:06 +0200

    Add payment routes to the server

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const paymentRoutes = require("./routes/paymentRoutes");
+
+app.use("/payments", paymentRoutes);
 
 require("dotenv").config();
 
