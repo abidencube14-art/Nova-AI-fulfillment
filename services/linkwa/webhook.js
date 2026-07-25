ff1ff2b721c580c1242f78695b35ec2b87223fff
abidencube14-art <abidencube14@gmail.com>
2026-07-25 16:04:37 +0200

    Add paymentWebhook function for handling webhooks

--- services/linkwa/webhook.js
+++ services/linkwa/webhook.js
@@ -1,1 +1,15 @@
+async function paymentWebhook(req, res) {
 
+    console.log("Webhook Received");
+
+    console.log(req.body);
+
+    res.status(200).json({
+        success: true
+    });
+
+}
+
+module.exports = {
+    paymentWebhook
+};
