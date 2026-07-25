99c3a7b833c2b850afe7d6ce5a08a76ef6a66ec4
abidencube14-art <abidencube14@gmail.com>
2026-07-25 16:03:33 +0200

    Implement createPaymentLink function for payments
    Added createPaymentLink function to handle payment link creation with error handling.

--- services/linkwa/payments.js
+++ services/linkwa/payments.js
@@ -1,1 +1,44 @@
+const axios = require("axios");
+const { getHeaders } = require("./linkwaAuth");
 
+const BASE_URL = process.env.LINKWA_BASE_URL;
+
+async function createPaymentLink(order) {
+    try {
+
+        const response = await axios.post(
+            `${BASE_URL}/payment-links`,
+            {
+                amount: order.amount,
+                currency_code: "USD",
+                payment_link_name: order.name,
+                description: order.description,
+                return_url: order.return_url,
+
+                autofill_contact_details: {
+                    full_name: order.customer_name,
+                    email: order.email,
+                    phone_number: order.phone
+                }
+            },
+            {
+                headers: getHeaders()
+            }
+        );
+
+        return response.data;
+
+    } catch (error) {
+
+        if (error.response) {
+            console.log(error.response.data);
+            throw error.response.data;
+        }
+
+        throw error;
+    }
+}
+
+module.exports = {
+    createPaymentLink
+};
