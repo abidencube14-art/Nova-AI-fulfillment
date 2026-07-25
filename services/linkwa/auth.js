d58240d0fd72960f05b95133d1ecb495298f2c3b
abidencube14-art <abidencube14@gmail.com>
2026-07-25 16:02:46 +0200

    Create Linkwa authentication service

--- services/linkwa/auth.js
+++ services/linkwa/auth.js
@@ -1,1 +1,13 @@
+const axios = require("axios");
 
+function getHeaders() {
+    return {
+        Authorization: `Bearer ${process.env.LINKWA_API_KEY}`,
+        Accept: "application/json",
+        "Content-Type": "application/json"
+    };
+}
+
+module.exports = {
+    getHeaders
+};
